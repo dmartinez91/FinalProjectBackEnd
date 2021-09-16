80b3e10aed75ff9778eb99c507a553c3a92d413c
dmartinez91 <dmartinez3691@gmail.com>
2021-09-16 00:40:02 -0400

    created authentication app urls files + register auth app urls in project URLS file

--- CSBackEnd/CSBackEnd/settings.py
+++ CSBackEnd/CSBackEnd/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'authentication.apps.AuthenticationConfig',
 ]
 
 MIDDLEWARE = [
@@ -115,6 +117,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',)
+}
+
 try:
     from CSBackEnd.local_settings import *
 except ImportError:
